5c8420b15d08b9686243698937394a1920aaaf85
nimesh <nkumarsachan@gmail.com>
2019-10-10 22:46:52 +0530

    Login Screen Code Added using phone number authentication

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,57 +1,165 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
+String countryCode = '+91';
+String phoneNumber = '';
+FirebaseAuth firebaseAuth;
+FirebaseUser firebaseUser;
+
 class _LoginPageState extends State<LoginPage> {
   TextEditingController phoneNumberController;
   TextEditingController smsOtpController;
-  int phoneNumber;
+
+  String verificationId = '';
+  String smsOtp = '';
+  FocusNode phoneNumberNode;
+
+  @override
+  void initState() {
+    super.initState();
+    phoneNumberNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    phoneNumberNode.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
     return Container(
       width: double.infinity,
       height: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+      child: Stack(
+//        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  boxShadow: [BoxShadow(blurRadius: 4.0, color: Colors.grey)]),
+              width: 80.0,
+              height: 80.0,
+              alignment: Alignment.center,
+              child: FlutterLogo(
+                duration: Duration(seconds: 3),
+                size: 40.0,
+                curve: Curves.bounceOut,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: TextField(
+                  focusNode: phoneNumberNode,
+                  autofocus: false,
+                  decoration: InputDecoration(labelText: 'Enter your phone number', border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+                  maxLength: 10,
+                  controller: phoneNumberController,
+                  onChanged: (value) {
+                    setState(() {
+                      print('onChanged: ' + value);
+                      phoneNumber = value;
+                    });
+                  },
+                  onSubmitted: (value) {
+                    print('phoneController: ' + value);
+                    setState(() {
+                      phoneNumber = value;
+                    });
+                  },
+                ),
+              ),
+              RaisedButton(
                 color: Colors.white,
-                boxShadow: [BoxShadow(blurRadius: 4.0, color: Colors.grey)]),
-            width: 80.0,
-            height: 80.0,
-            alignment: Alignment.center,
-            child: FlutterLogo(
-              duration: Duration(seconds: 3),
-              size: 40.0,
-              curve: Curves.bounceOut,
-            ),
-          ),
-          Container(
-            child: TextField(
-              autofocus: true,
-              decoration: InputDecoration(labelText: 'Enter your phone number'),
-              keyboardType: TextInputType.number,
-              maxLength: 10,
-              controller: phoneNumberController,
-              onSubmitted: (value) {
-                print('phoneController: ' + value);
-              },
-            ),
-          ),
-          RaisedButton(
-            onPressed: () {
+                shape: StadiumBorder(),
+                onPressed: () {
+                  if (phoneNumber.trim().isEmpty || phoneNumber.trim().length < 10) {
+                    print('Please Enter 10 digit phone number');
+                    Fluttertoast.showToast(msg: 'Please Enter 10 digit phone number');
+                  } else {
+                    print('verify phone number called...');
 
-            },
-            child: Text('Login'),
-          )
+                    /// calling firebase functions for authentication
+                    /// sends the code to the specified phone number
+                    sendCodeToPhoneNumber();
+                  }
+                },
+                child: Text('Login'),
+              )
+            ],
+          ),
         ],
       ),
     );
+  }
+
+  Future<void> sendCodeToPhoneNumber() async {
+    final PhoneVerificationCompleted verificationCompleted = (AuthCredential authCredential) async {
+      print('phoneVerificationCompleted called.\n');
+      print('authCredential: ' + authCredential.toString());
+      print('phoneVerification auto succeeded:');
+      await FirebaseAuth.instance.signInWithCredential(authCredential).then((AuthResult user) {
+        setState(() {
+          firebaseUser = user.user;
+          print('phoneVerification auto succeeded with user:' + user.user.toString());
+        });
+      }).catchError((onError) {
+        setState(() {
+          print('catchError phoneVerficationCompleted: ' + onError.toString());
+        });
+      });
+    };
+
+    final PhoneVerificationFailed phoneVerificationFailed = (AuthException authException) {
+      setState(() {
+        print('phoneVerificationFailed called...');
+        print('phoneVerificationFailed code: ' + authException.code.toString());
+      });
+    };
+
+    final PhoneCodeSent phoneCodeSent = (String verificationId, [int forceResendingToken]) async {
+      setState(() {
+        print('phoneCodeSent Called...');
+        print('verificationId: ' + verificationId + ' forceResendingToken: ' + forceResendingToken.toString());
+        verificationId = verificationId;
+      });
+    };
+
+    final PhoneCodeAutoRetrievalTimeout phoneCodeAutoRetrievalTimeout = (String verificationId) {
+      setState(() {
+        print('phoneCodeAutoRetrievalTimeout Called');
+        print('verficationId: ' + verificationId);
+        print('timeOut');
+      });
+    };
+
+    await FirebaseAuth.instance
+        .verifyPhoneNumber(
+            phoneNumber: countryCode + phoneNumber,
+            timeout: Duration(seconds: 30),
+            verificationCompleted: verificationCompleted,
+            verificationFailed: phoneVerificationFailed,
+            codeSent: phoneCodeSent,
+            codeAutoRetrievalTimeout: phoneCodeAutoRetrievalTimeout)
+        .catchError((onError) {
+      print('catchError verifyPhoneNumber: ' + onError.toString());
+    });
   }
 }
